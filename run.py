dc9eb6ab6e647a0213e3bde07c6e31457dfae071
Petar Pavlovic <petar.pavlovic.37@gmail.com>
2025-04-15 12:23:56 +0200

    [fix] ...

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--client', '-c', dest='client', type=str, default=None)
-    parser.add_arguement('--port', '-p', dest='port', type=int, default=7024)
+    parser.add_argument('--port', '-p', dest='port', type=int, default=7024)
     parser.add_argument('--server', '-s', dest='server', action='store_true')
 
     args = parser.parse_args()
