417314de6a858b60bd2f7ea53e39e5b992962b3b
Petar Pavlovic <petar.pavlovic.37@gmail.com>
2025-04-23 13:04:18 +0200

    [update] record frames rotations

--- record_frames.py
+++ record_frames.py
@@ -2,7 +2,7 @@ import cv2
 import time
 import argparse
 from picamera2 import Picamera2
-from libcamera import controls
+from libcamera import controls, Transform
 from lib.utils.timestamp import get_time
 
 
@@ -14,6 +14,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--name', dest='name', type=str)
+    parser.add_argument('-r', '--rotate', dest='rotate', ) # ToDo not used yet
 
     args = parser.parse_args()
 
@@ -22,12 +23,17 @@ if __name__ == '__main__':
         'AfMode': controls.AfModeEnum.Manual,
         'LensPosition': 0.0
     })
-
+    config = cam.create_still_configuration(transform=Transform(hflip=True, vflip=True))
+    cam.configure(config)
     cam.start()
 
+    start = time.time()
+
     while True:
-        time.sleep(1)
-
-        image = cam.capture_array('main')
-
-        save_image(image, args.name)
+        if time.time() - start > 1:
+            image = cam.capture_array('main')
+            # image = cv2.rotate(image, cv2.ROTATE_180_CLOCKWISE)
+            save_image(image, args.name)
+            start = time.time()
+        else:
+            time.sleep(0.1)
